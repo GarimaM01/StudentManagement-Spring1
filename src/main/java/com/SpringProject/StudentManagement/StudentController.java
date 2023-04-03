0ef5df6c2d02b97787b2a5cf0e15ec597f8f67ed
GarimaM01 <garima.mahto@sakec.ac.in>
2023-04-03 10:24:03 +0530

    StudentManagement-Spring1

---
+++ src/main/java/com/SpringProject/StudentManagement/StudentController.java
@@ -0,0 +1,37 @@
+package com.SpringProject.StudentManagement;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+public class StudentController {
+
+    Map<Integer,Student> db = new HashMap<>();
+
+    @GetMapping("/get_info")
+    Student getStudent(@RequestParam("id") int admNo){
+        return db.get(admNo);
+    }
+
+    @PostMapping("/add")
+    String addStudent(@RequestBody() Student student){
+        db.put(student.getAdmNo(),student);
+        return "Student has been add Successfully";
+    }
+    @DeleteMapping("/delete/{id}")
+    String deleteStudent(@PathVariable("id") int admNo){
+        db.remove(admNo);
+        return "Student Deleted";
+    }
+
+    @PutMapping("/update")
+    Student updateStudent(@RequestParam("id") int admNo,Student student){
+        update(student);
+        return db.get(admNo);
+    }
+    void update(@RequestBody Student student){
+        db.put(student.getAdmNo(),student);
+        System.out.println("Student Data is Updated");
+    }
+}
